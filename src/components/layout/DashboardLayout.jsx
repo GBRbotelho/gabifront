5afa4200251fc523d1c2310cdd47040341bd8091
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-12 18:48:18 -0300

    Layout SideBar

--- src/components/layout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../menus/Navbar";
+import Sidebar from "../menus/Sidebar"; // Importe o Sidebar aqui
 
 function DashboardLayout({ children }) {
   return (
     <div>
-      <Navbar />
-      <main>{children}</main>
+      <Navbar /> {/* Navbar no topo */}
+      <div className="flex">
+        <Sidebar /> {/* Sidebar à esquerda */}
+        <main>{children}</main> {/* Conteúdo principal */}
+      </div>
     </div>
   );
 }
