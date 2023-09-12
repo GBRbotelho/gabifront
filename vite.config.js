e6c92c4c9790fd7f45532a438347c203c0dacaff
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-12 12:24:13 -0300

    Config Rota

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.0.100", // Substitua pelo seu endereço IP local
+    host: "localhost",
     port: 5173,
   },
 });
