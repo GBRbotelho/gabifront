5bd7bd2e179c8ea8e8c0d795061e941c270746f5
GBRbotelho <gabrielbbotelho05@gmail.com>
2023-09-13 13:33:40 -0300

    Correção menu hamburguer

--- src/components/menus/Navbar.jsx
+++ src/components/menus/Navbar.jsx
@@ -15,24 +15,25 @@ function Navbar({ isSidebarOpen, toggleSidebar }) {
       <div className="h-[61px] bg-custom px-3 py-2 print:hidden lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-start">
-            {/* Sempre exiba o botão do menu, não importa o tamanho da tela */}
-            <button
-              onClick={handleToggleSidebar}
-              className="text-white hover:text-gray-300 focus:outline-none"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
+              <div className="lg:hidden">
+              <button
+                onClick={handleToggleSidebar}
+                className="text-white hover:text-gray-300 focus:outline-none"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
             <a to="/" className="ml-2 flex md:mr-24">
               <span className="self-center whitespace-nowrap text-2xl font-semibold text-white">
                 ByteWave
