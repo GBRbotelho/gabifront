d78f1a87b68f7078d5db0728859e6957e1b64189
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-10-04 23:00:08 -0300

    Fetch Clients

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,24 +1,21 @@
 export async function fetchClients() {
   try {
-    const response = await fetch("http://localhost:3000/api/clients", {
+    const token = await localStorage.getItem("token");
+    const response = await fetch("http://localhost:3000/clients", {
       method: "GET",
       headers: {
-        // Adicione quaisquer cabeçalhos de autenticação ou outros cabeçalhos necessários aqui
         "Content-Type": "application/json",
-        // Exemplo de cabeçalho de autenticação (se aplicável)
-        // "Authorization": "Bearer seu-token-de-autenticacao",
+        Authorization: `${token}`,
       },
     });
 
     if (!response.ok) {
-      // Lida com erros de resposta da API, se necessário
       throw new Error("Erro ao buscar clientes");
     }
 
     const data = await response.json();
     return data;
   } catch (error) {
-    // Lida com erros de rede ou outros erros
     throw new Error("Erro na solicitação de clientes: " + error.message);
   }
 }
