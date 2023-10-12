a22edb41f600fdcd93b9d656846a37f974fdae8d
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-10-11 23:13:37 -0300

    Registration Form #3

--- src/components/forms/RegistrationForm.jsx
+++ src/components/forms/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function RegistrationForm({ closeModal }) {
   const [isEditable, setIsEditable] = useState(false);
@@ -13,180 +13,135 @@ function RegistrationForm({ closeModal }) {
             <h2 className="text-2xl font-semibold">Ficha Cadastral</h2>
           </div>
           <div className="max-h-80 overflow-y-auto">
-          <div className="flex items-center mb-4">
-            <h2 className="text-2xl text-gray-500 font-semibold">
-              Estilo de vida
-            </h2>
-          </div>
-
-          <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-4">
-            <div className="md:col-span-2">
-              <label htmlFor="full_name">Nome Completo</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-              />
+            <div className="flex items-center mb-4">
+              <h2 className="text-2xl text-gray-500 font-semibold">
+                Estilo de vida
+              </h2>
             </div>
 
-            <div className="md:col-span-2">
-              <label htmlFor="country">CPF</label>
-              <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-4">
+              <div className="md:col-span-2">
+                <label htmlFor="full_name">Estado Civil</label>
                 <input
-                  name="cpf"
-                  id="cpf"
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                />
+              </div>
+
+              <div className="md:col-span-2">
+                <label htmlFor="country">Profissão</label>
+                <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                  <input
+                    name="cpf"
+                    id="cpf"
+                    placeholder="Preencha este campo"
+                    className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                    disabled={!isEditable}
+                  />
+                </div>
+              </div>
+
+              <div className="md:col-span-2">
+                <label htmlFor="state">Atividade Fisica</label>
+                <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                  <input
+                    name="phone"
+                    id="phone"
+                    placeholder="Preencha este campo"
+                    className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                  />
+                </div>
+              </div>
+
+              <div className="md:col-span-2">
+                <label htmlFor="zipcode">Peso</label>
+                <input
+                  type="number"
+                  name="gender"
+                  id="gender"
+                  className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                   placeholder="Preencha este campo"
-                  className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
                   disabled={!isEditable}
                 />
               </div>
-            </div>
-
-            <div className="md:col-span-2">
-              <label htmlFor="state">Telefone</label>
-              <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+              <div className="md:col-span-2">
+                <label htmlFor="email">Habitos alimentares/social</label>
                 <input
-                  name="phone"
-                  id="phone"
+                  type="date"
+                  name="data"
+                  id="date"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                />
+              </div>
+
+              <div className="md:col-span-2">
+                <label htmlFor="email">Medicamentos contínuos</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                  placeholder="email@domain.com"
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="address">Problemas de saúde</label>
+                <input
+                  type="text"
+                  name="street"
+                  id="street"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                   placeholder="Preencha este campo"
-                  className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
                 />
               </div>
             </div>
-
-            <div className="md:col-span-2">
-              <label htmlFor="zipcode">Sexo</label>
-              <input
-                type="text"
-                name="gender"
-                id="gender"
-                className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                placeholder="Preencha este campo"
-                disabled={!isEditable}
-              />
+            <div className="flex items-center mb-4 mt-6">
+              <h2 className="text-2xl text-gray-500 font-semibold">
+                Expectativas
+              </h2>
             </div>
-            <div className="md:col-span-2">
-              <label htmlFor="email">Data de Nascimento</label>
-              <input
-                type="date"
-                name="data"
-                id="date"
-                className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-              />
-            </div>
-
-            <div className="md:col-span-2">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                placeholder="email@domain.com"
-                disabled={!isEditable}
-              />
-            </div>
-            <div className="md:col-span-2">
-              <label htmlFor="address">Rua</label>
-              <input
-                type="text"
-                name="street"
-                id="street"
-                className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                placeholder="Preencha este campo"
-              />
-            </div>
-
-            <div className="md:col-span-2">
-              <label htmlFor="city">Bairro</label>
-              <input
-                type="text"
-                name="district"
-                id="district"
-                className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                placeholder="Preencha este campo"
-              />
-            </div>
-
-            <div className="md:col-span-2">
-              <label htmlFor="zipcode">Nº</label>
-              <input
-                type="text"
-                name="number"
-                id="number"
-                className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                placeholder="Preencha este campo"
-              />
-            </div>
-
-            <div className="md:col-span-2">
-              <label htmlFor="zipcode">CEP</label>
-              <input
-                type="text"
-                name="cep"
-                id="cep"
-                className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                placeholder="Preencha este campo"
-              />
-            </div>
-
-            <div className="md:col-span-2">
-              <label htmlFor="country">Cidade</label>
-              <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-4">
+              <div className="md:col-span-2">
+                <label htmlFor="full_name">Como posso te ajudar</label>
                 <input
-                  name="city"
-                  id="city"
-                  placeholder="Preencha este campo"
-                  className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                 />
               </div>
             </div>
+          </div>
+          <div className="flex justify-between mt-3">
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={closeModal}
+            >
+              Fechar
+            </button>
 
-            <div className="md:col-span-2">
-              <label htmlFor="state">Estado</label>
-              <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
-                <input
-                  name="state"
-                  id="state"
-                  placeholder="Preencha este campo"
-                  className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
-                />
-              </div>
-            </div>
-
-            <div className="md:col-span-2 text-right">
-              <div className="inline-flex items-end gap-1">
+            <div className="">
+              <div className="">
                 {isEditable ? (
                   <>
-                    <button
-                      className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                    >
+                    <button className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                       <i className="ri-close-fill"></i>Cancel
                     </button>
-                    <button
-                      className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                    >
+                    <button className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                       <i className="ri-save-line"></i>Save
                     </button>
                   </>
                 ) : (
-                  <button
-                    className=" bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
-                  >
+                  <button className=" bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
                     <i className="ri-pencil-fill"> </i>Edit
                   </button>
                 )}
               </div>
             </div>
           </div>
-          </div>
-          <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            onClick={closeModal}
-          >
-            Fechar
-          </button>
         </div>
       </div>
     </div>
