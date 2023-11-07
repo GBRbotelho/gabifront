443b4540fe94534908a6b9c886c98da3f66dff41
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-11-06 22:01:39 -0300

    Redirecionamento de Pagina

--- src/pages/ConfirmationEmailPage.jsx
+++ src/pages/ConfirmationEmailPage.jsx
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext";
+import { usePostData } from "../services/apiService";
 
 export default function ConfirmationEmailPage() {
   const navigate = useNavigate();
-  const { token, isLoading, user } = useAuth();
+  const { isLoading, user } = useAuth();
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    // Verifique se o token ou o usuário é nulo
+    navigate("/");
+  }
+  useEffect(() => {
+    console.log(user);
+    if (user) {
+      if (user.isEmailVerified) {
+        navigate("/dashboard/clientes");
+      }
+    }
+  }, [user]);
 
   if (isLoading) {
     // Renderize algum indicador de carregamento, se necessário
     return <div>Carregando...</div>;
   }
 
-  if (!token || !user) {
-    navigate("/");
-  } else if (token && user.isEmailVerified) {
-    navigate("/dashboard/clientes");
-  }
   return (
     <div className="bg-emerald-300">
-      <div class="app font-sans min-w-screen min-h-screen bg-grey-lighter py-8 px-4">
-        <div class="mail__wrapper max-w-md mx-auto">
-          <div class="mail__content bg-white p-8 shadow-md">
-            <div class="content__header text-center tracking-wide border-b">
-              <div class="text-emerald-500 text-sm font-bold">BYTEWAVE</div>
-              <h1 class="text-3xl h-48 flex items-center justify-center font-bold">
+      <div className="app font-sans min-w-screen min-h-screen bg-grey-lighter py-8 px-4">
+        <div className="mail__wrapper max-w-md mx-auto">
+          <div className="mail__content bg-white p-8 shadow-md">
+            <div className="content__header text-center tracking-wide border-b">
+              <div className="text-emerald-500 text-sm font-bold">BYTEWAVE</div>
+              <h1 className="text-3xl h-48 flex items-center justify-center font-bold">
                 Confirme seu Email
               </h1>
             </div>
 
-            <div class="content__body py-8 border-b">
+            <div className="content__body py-8 border-b">
               <p>
                 Ola, <br />
                 Pedimos que faça a autenticação de sua conta digitando o codigo
@@ -40,14 +50,14 @@ export default function ConfirmationEmailPage() {
                 maxLength="6"
                 placeholder="Insira seu código"
               />
-              <button class="text-white text-sm tracking-wide bg-green-500 rounded w-full my-8 p-4 ">
+              <button className="text-white text-sm tracking-wide bg-green-500 rounded w-full my-8 p-4 ">
                 CONFIRMAR CODIGO
               </button>
-              <p class="text-sm">Em breve você podera usar o sistema!</p>
+              <p className="text-sm">Em breve você podera usar o sistema!</p>
             </div>
 
-            <div class="content__footer mt-8 text-center text-grey-darker">
-              <h3 class="text-base sm:text-lg mb-4">
+            <div className="content__footer mt-8 text-center text-grey-darker">
+              <h3 className="text-base sm:text-lg mb-4">
                 Obrigado por escolher Bytewave!
               </h3>
             </div>
