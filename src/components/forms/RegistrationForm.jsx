d18612e738075e2b66bee82904a467259f1da037
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-12-13 20:13:11 -0300

    Att registration form

--- src/components/forms/RegistrationForm.jsx
+++ src/components/forms/RegistrationForm.jsx
@@ -338,6 +338,264 @@ function RegistrationForm({ closeModal }) {
                   disabled={!isEditable}
                 />
               </div>
+
+              <div className="md:col-span-2">
+                <label htmlFor="lastDate">
+                  Se travou, qual foi a ultima vez
+                </label>
+                <input
+                  type="date"
+                  name="lastDate"
+                  id="lastDate"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.lastDate || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="respiratoryPain">
+                  Dor ao respirar, tossir ou espirrar
+                </label>
+                <select
+                  type="text"
+                  name="respiratoryPain"
+                  id="respiratoryPain"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.respiratoryPain || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                >
+                  <option value="" hidden></option>
+                  <option value="Sim">Sim</option>
+                  <option value="Não">Não</option>
+                </select>
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="vertigoSymptoms">
+                  Vertigem, zumbido ou nausea
+                </label>
+                <select
+                  type="text"
+                  name="vertigoSymptoms"
+                  id="vertigoSymptoms"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.vertigoSymptoms || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                >
+                  <option value="" hidden></option>
+                  <option value="Sim">Sim</option>
+                  <option value="Não">Não</option>
+                </select>
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="strengthLoss">Perda de força</label>
+                <select
+                  type="text"
+                  name="strengthLoss"
+                  id="strengthLoss"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.strengthLoss || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                >
+                  <option value="" hidden></option>
+                  <option value="Sim">Pernas</option>
+                  <option value="Não">Braços</option>
+                </select>
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="urinarySystem">
+                  Sistema urinario e fecal funcionando
+                </label>
+                <input
+                  type="text"
+                  name="urinarySystem"
+                  id="urinarySystem"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.urinarySystem || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="landmark">Sofreu algum evento marcante</label>
+                <input
+                  type="text"
+                  name="landmark"
+                  id="landmark"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.landmark || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="procedures">Tem cirurgias</label>
+                <input
+                  type="text"
+                  name="procedures"
+                  id="procedures"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.procedures || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="sleepQuality">Qualidade de sono</label>
+                <select
+                  type="text"
+                  name="sleepQuality"
+                  id="sleepQuality"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.sleepQuality || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                >
+                  <option value="" hidden></option>
+                  <option value="Bom">Bom</option>
+                  <option value="Médio">Médio</option>
+                  <option value="Ruim">Ruim</option>
+                </select>
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="anxietyStress">Ansiedade ou estresse</label>
+                <select
+                  type="text"
+                  name="anxietyStress"
+                  id="anxietyStress"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.anxietyStress || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                >
+                  <option value="" hidden></option>
+                  <option value="Sim">Sim</option>
+                  <option value="Não">Não</option>
+                </select>
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="activityPause">
+                  Deixou de fazer atividades
+                </label>
+                <input
+                  type="text"
+                  name="activityPause"
+                  id="activityPause"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.activityPause || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="posture">Postura</label>
+                <input
+                  type="text"
+                  name="posture"
+                  id="posture"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.posture || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="assessmentConclusion">
+                  Conclusão da avaliação
+                </label>
+                <input
+                  type="text"
+                  name="assessmentConclusion"
+                  id="assessmentConclusion"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.assessmentConclusion || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="painLocation">Onde sente dor</label>
+                <select
+                  type="text"
+                  name="painLocation"
+                  id="painLocation"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.painLocation || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                >
+                  <option value="" hidden></option>
+                  <option value="Costas parte inferior">
+                    Costas parte inferior
+                  </option>
+                  <option value="Costas parte superior">
+                    Costas parte superior
+                  </option>
+                  <option value="Costas meio escapulas">
+                    Costas meio escapulas
+                  </option>
+                  <option value="Pescoço parte superior">
+                    Pescoço parte superior
+                  </option>
+                  <option value="Pescoço laterais">Pescoço laterais</option>
+                  <option value="Ombro">Ombro</option>
+                  <option value="Pés">Pés</option>
+                  <option value="Coxa">Coxa</option>
+                  <option value="Panturrilha">Panturrilha</option>
+                  <option value="Posterior de coxa">Posterior de coxa</option>
+                  <option value="Mãos">Mãos</option>
+                  <option value="Braços">Braços</option>
+                  <option value="Cabeça">Cabeça</option>
+                  <option value="Outro">Outro</option>
+                </select>
+              </div>
+              {registrationForm.painLocation === "Outro" && (
+                <div className="md:col-span-2">
+                  <label htmlFor="otherLocation">
+                    Outra localização da dor
+                  </label>
+                  <input
+                    type="text"
+                    name="otherLocation"
+                    id="otherLocation"
+                    className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                      !isEditable ? "gray-100" : "white"
+                    }`}
+                    value={registrationForm.otherLocation || ""}
+                    onChange={handleChange}
+                    disabled={!isEditable}
+                  />
+                </div>
+              )}
               <div className="md:col-span-2">
                 <label htmlFor="treatments">Tratamentos anteriores</label>
                 <input
@@ -348,6 +606,55 @@ function RegistrationForm({ closeModal }) {
                     !isEditable ? "gray-100" : "white"
                   }`}
                   value={registrationForm.treatments || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+            </div>
+            <div className="flex items-center mb-4 mt-6">
+              <h2 className="text-2xl text-gray-500 font-semibold">
+                Plano definido
+              </h2>
+            </div>
+            <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-4">
+              <div className="md:col-span-2">
+                <label htmlFor="sessionsNumber">Numero de sessões</label>
+                <input
+                  type="text"
+                  name="sessionsNumber"
+                  id="sessionsNumber"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.sessionsNumber || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="method">Técnica</label>
+                <input
+                  type="text"
+                  name="method"
+                  id="method"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.method || ""}
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </div>
+              <div className="md:col-span-2">
+                <label htmlFor="price">Preço</label>
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  className={`h-10 border mt-1 rounded px-4 w-full bg-${
+                    !isEditable ? "gray-100" : "white"
+                  }`}
+                  value={registrationForm.price || ""}
                   onChange={handleChange}
                   disabled={!isEditable}
                 />
